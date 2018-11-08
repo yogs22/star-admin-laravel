4a8b19e1bae0432be2508af46589ba0bad0ab928
Piotr Stankiewicz <stankiewicz.opole@gmail.com>
2018-11-08 15:53:36 +0100

    add  user to dashboard partials by view composer

--- app/Modules/Dashboard/Providers/DashboardModuleServiceProvider.php
+++ app/Modules/Dashboard/Providers/DashboardModuleServiceProvider.php
@@ -2,9 +2,18 @@
 
 namespace App\Modules\Dashboard\Providers;
 
+use Illuminate\Support\Facades\Auth;
+
 class DashboardModuleServiceProvider extends AbstractModuleServiceProvider
 {
     protected $namespace = 'App\Modules\Dashboard\Http\Controllers';
 
     protected $name = 'dashboard';
+
+    public function bootModule()
+    {
+        view()->composer('dashboard::partials.*', function ($view) {
+            $view->with('user', Auth::user());
+        });
+    }
 }
